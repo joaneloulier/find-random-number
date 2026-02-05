15cb8ae87ce98696582483913795cf1ba684383c
Joane Loulier <joane.loulier@cea.fr>
2026-02-05 17:20:48 +0100

    creation of my 2 tabs : game_tab to actually play the game and login_tab to enter the user name

---
+++ gui/game_tab.py
@@ -0,0 +1,6 @@
+from PySide6.QtWidgets import QWidget
+
+
+class GameTab(QWidget):
+    def __init__(self):
+        super().__init__()
---
+++ gui/login_tab.py
@@ -0,0 +1,6 @@
+from PySide6.QtWidgets import QWidget
+
+
+class LoginTab(QWidget):
+    def __init__(self):
+        super().__init__()
