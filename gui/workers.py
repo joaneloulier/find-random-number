84b87101e7d96df4c41d6a99d01412fb11a3800b
Joane Loulier <joane.loulier@cea.fr>
2026-02-06 18:11:23 +0100

    worker for my application, communicates bw backend and frontent and orchestrates the frontend

---
+++ gui/workers.py
@@ -0,0 +1,11 @@
+from PySide6.QtWidgets import QWidget
+
+"""
+Classe qui va me servir à faire le pont entre l'interface et le backend.
+
+Dès que le username clique sur 
+
+"""
+class Worker(QWidget):
+    def __init__(self):
+        super().__init__()
