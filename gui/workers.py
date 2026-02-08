7618209c78fd0e6ee79d4e37150ae6547c35de24
Joane Loulier <joane.loulier@cea.fr>
2026-02-08 18:52:23 +0100

    created the file workers.py to connect with the backend

--- gui/workers.py
+++ gui/workers.py
@@ -3,9 +3,15 @@ from PySide6.QtWidgets import QWidget
 """
 Classe qui va me servir à faire le pont entre l'interface et le backend.
 
-Dès que le username clique sur 
+1. Dès que le username clique sur commencer le jeu, le jeu est lancé, 
+le backend envoie le nombre à deviner.
+2. le worker récupère le nombre devine et utilise les fcts du backend pour
+savoir si c'est le bon et lance les messages en fonction de si le nombre
+est plus ou plus petit.
 
 """
 class Worker(QWidget):
     def __init__(self):
         super().__init__()
+
+        AppSignals.instance().
