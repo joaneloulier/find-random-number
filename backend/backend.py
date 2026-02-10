04bd1bd721b16ccecce9a56f265d96f5c0ed3bdb
Joane Loulier <joane.loulier@cea.fr>
2026-02-10 16:40:09 +0100

    change of function name to be more explicit

--- backend/backend.py
+++ backend/backend.py
@@ -1,15 +1,15 @@
 import random
 
+
 def generate_number(start_int: int = 0, end_int: int = 100):
     return random.randint(start_int, end_int)
+
 
 def correct_answer(solution: int, guess: int):
     if solution == guess:
         return True
     return False
 
-def higher_or_lower(solution: int, guess: int):
-    if solution > guess:
-        return 'guess_lower_than_sol'
-    else:
-        return 'guess_higher_than_sol'
+
+def guess_lower_than_sol(solution: int, guess: int):
+    return solution > guess  # true if guess is lower than sol
