2ad2728808186aaea6c485837502d4e087662368
Joane Loulier <joane.loulier@cea.fr>
2026-02-10 16:39:45 +0100

    signals emitted depending on the comparison bw guess and sol

--- gui/workers.py
+++ gui/workers.py
@@ -9,9 +9,26 @@ le backend envoie le nombre à deviner.
 savoir si c'est le bon et lance les messages en fonction de si le nombre
 est plus ou plus petit.
 
+Pourquoi j'ai encore tant de mal avec les imports ?? ENFER.
+
 """
+
+from .signals import AppSignals
+from backend import backend
+
+
 class Worker(QWidget):
     def __init__(self):
         super().__init__()
+        AppSignals.instance().GuessSignal.connect(self.on_guess_received)
+        self.solution = backend.generate_number()
 
-        AppSignals.instance().
+    def on_guess_received(self, guess: int):
+        print("guess received")
+        if backend.correct_answer(self.solution, guess):
+            pass  # TODO : on va à l'onglet suivant
+        else:
+            if backend.guess_lower_than_sol(self.solution, guess):
+                AppSignals.instance().ComparisonGuessSol.emit("lower")
+            else:
+                AppSignals.instance().ComparisonGuessSol.emit("higher")
